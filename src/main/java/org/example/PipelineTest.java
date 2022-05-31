e1e4f1b1d077e4a889db9f0b90420db8f4ee8dab
Kan Cheng <kan.bk.cheng@gtomato.com>
2022-05-31 16:27:14 +0800

    Javapipeline version 1

---
+++ src/main/java/org/example/PipelineTest.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class PipelineTest {
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello, this is Pipeline Tester !" );
+    }
+
+}
